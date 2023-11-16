ffb9c3d8c9d40d3ef37d8060094f7a1e2f86b317
simmihugs <simon.graetz@gmx.de>
2023-11-16 15:33:57 +0100

    Update error counter for table

--- src/pts_loader/special_event.rs
+++ src/pts_loader/special_event.rs
@@ -181,126 +181,17 @@ impl<'a> SpecialEvent<'a> {
         let mut logoerror = 0;
         let mut iderror = 0;
 
-        /*
-        let layout_events: Vec<_> = self
-            .vec
-            .iter()
-            .filter(|x| match x {
-                Define::layoutEvent(..) => true,
-                _ => false,
-            })
-            .collect();
-
-        let logo_events: Vec<_> = self
-            .vec
-            .iter()
-            .filter(|x| match x {
-                Define::logoEvent(..) => true,
-                _ => false,
-            })
-            .collect();
-        */
-
+        let debug_me = false;
         let mut found_first_event: bool = false;
         let mut found_dran_bleiben: bool = false;
         for s in &self.vec {
             match s {
                 Define::vaEvent(event) => {
-                    let (logos, mut logostr) = self.find_logo_str(event);
-                    /*
-                    let mut layouts = Vec::new();
-                    let mut logos = Vec::new();
-                    for layout in &layout_events {
-                        if event.get_starttime() == layout.get_event().get_starttime() {
-                            if event.get_endtime() != layout.get_event().get_endtime() {
-                                logoerror += 1;
-                                layouts.push((true, layout));
-                            } else {
-                                layouts.push((false, layout));
-                            }
-                        }
-                    }
-                    for logo in &logo_events {
-                        if event.get_starttime() <= logo.get_event().get_starttime()
-                            && logo.get_event().get_starttime() <= event.get_endtime()
-                        {
-                            if event.get_endtime() <= logo.get_event().get_endtime() {
-                                logoerror += 1;
-                                logos.push((true, logo));
-                            } else {
-                                logos.push((false, logo));
-                            }
-                        }
-                    }
-
-                    let mut logostr = String::new();
-                    if event.get_contentid() == "cb7a119f84cb7b117b1b"
-                        || event.get_contentid() == "392654926764849cd5dc"
-                        || event.get_title().contains("Black")
-                        || event.get_title().contains("NK_Maschinen_der_Superlat")
-                        || event.get_contentid().contains("WERBUNG")
-                        || event.get_duration() < 60_0000
-                    {
-                        logostr = format!("{}", "");
-                    } else if event.get_contentid().contains("UHD_IN2") {
-                        for (_error, logo) in &logos {
-                            if logo.get_event().get_logo().contains("NO LOGO") {
-                                logostr = format!("{}", "");
-                            } else {
-                                logostr += &format!("ERROR {}", logo.get_event().get_logo());
-                            }
-                        }
-                        if logostr.is_empty() {
-                            for (_error, layout) in &layouts {
-                                if layout.get_event().get_logo().contains("NO LOGO") {
-                                    logostr = format!("{}", "");
-                                } else {
-                                    logostr += &format!("ERROR {}", layout.get_event().get_logo());
-                                }
-                            }
-                        }
-                    } else {
-                        for (error, logo) in &logos {
-                            if *error {
-                                logostr += &format!("ERROR {}", logo.get_event().get_logo());
-                            } else {
-                                logostr += &format!("{}", logo.get_event().get_logo());
-                            }
-                        }
-                        for (error, layout) in &layouts {
-                            if *error {
-                                logostr += &format!("ERROR {}", layout.get_event().get_logo());
-                            } else {
-                                logostr += &format!("{}", layout.get_event().get_logo());
-                            }
-                        }
-
-                        if logostr.len() == 0 {
-                            logostr = format!("{}", "ERROR_NO_LOGO");
-                        } else if logostr.chars().count() > 20 {
-                            logostr = format!(
-                                "{}",
-                                logostr
-                                    .chars()
-                                    .into_iter()
-                                    .enumerate()
-                                    .filter(|(i, _)| *i < 20)
-                                    .fold(String::new(), |mut acc, (_, c)| {
-                                        acc += &format!("{}", c);
-                                        return acc;
-                                    })
-                            );
-                        }
-                    }
-                     */
+                    let (_, mut logostr) = self.find_logo_str(event);
 
                     let contentid = event.get_contentid();
                     if contentid.contains("-") && contentid != "UHD1_WERBUNG-01" {
                         iderror += 1;
-                    }
-                    if logostr.contains("ERROR") && logoerror == 0 && contentid != "UHD1_WERBUNG-01"
-                    {
-                        logoerror = 1;
                     }
 
                     let mut title = event.title_to_string();
@@ -351,8 +242,9 @@ impl<'a> SpecialEvent<'a> {
                                 contentid.red().clear()
                             },
                             if logostr.contains("ERROR") && contentid != "UHD1_WERBUNG-01" {
-                                logostr.red()
-                            } else if logos.len() > 1 {
+                                if debug_me {
+                                    println!("{}", "found error");
+                                }
                                 logoerror += 1;
                                 logostr.red()
                             } else {
@@ -360,41 +252,6 @@ impl<'a> SpecialEvent<'a> {
                             },
                         );
                     }
-                    /*
-                    if verbose {
-                        println!(
-                            "| {:30} | {:15} | {:23} | {:23} | {:12} | {:20} | {:15} |",
-                            title,
-                            event.programid_to_string(),
-                            SpecialEvent::color_title(
-                                event,
-                                &mut found_first_event,
-                                &mut found_dran_bleiben,
-                                utc,
-                                fps
-                            ),
-                            event.endtime_to_string(utc, fps),
-                            if title == "Werbung" {
-                                event.duration_to_string(fps).yellow()
-                            } else {
-                                event.duration_to_string(fps).yellow().clear()
-                            },
-                            if contentid.contains("-") && contentid != "UHD1_WERBUNG-01" {
-                                contentid.red()
-                            } else {
-                                contentid.red().clear()
-                            },
-                            if logostr.contains("ERROR") && contentid != "UHD1_WERBUNG-01" {
-                                logostr.red()
-                            } else if logos.len() + layouts.len() > 1 {
-                                logoerror += 1;
-                                logostr.red()
-                            } else {
-                                logostr.red().clear()
-                            },
-                        );
-                    }
-                     */
                 }
                 _ => (),
             }
